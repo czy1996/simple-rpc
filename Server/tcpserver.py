cacab239ae917c7cdb14a39756c08703f256273f
MRROBOT\nicai <czy1996@users.noreply.github.com>
2017-06-28 20:15:55 +0800

    tcpserver

--- Server/tcpserver.py
+++ Server/tcpserver.py
@@ -5,9 +5,20 @@ class Server:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def bind(self, host='localhost', port='5000'):
+    def bind(self, host='localhost', port=5000):
         self.sock.bind((host, port))
         self.sock.listen(5)
 
     def accept_receive_close(self):
         (client_socket, address) = self.sock.accept()
+        msg = client_socket.recv(1024)
+        # self.on_msg(msg)
+        print(msg.decode())
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.bind()
+    while True:
+        server.accept_receive_close()
